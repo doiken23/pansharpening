802c73b97c01a2cfc0ad04b1ebc146817bd37a6e
Doi Kento <spreadcats.nya@gmail.com>
2018-09-09 16:29:23 +0900

    [fix] input dimension

--- test/test_model.py
+++ test/test_model.py
@@ -17,13 +17,14 @@ class TestModel(unittest.TestCase):
         self.assertEqual(outputs.size(), expected_size)
 
     def test_resnet(self):
-        net1 = resnet18(4, 3)
-        net2 = resnet18(4, 3, batch_norm=False)
-        inputs1 = torch.rand(2, 3, 512, 512)
-        inputs2 = torch.rand(2, 1, 512, 512)
-        for net in [net1, net2]:
-            outputs = net(inputs1, inputs2)
-            self.assertEqual(inputs1.size(), outputs.size())
+        inputs1 = torch.rand(2, 3, 128, 128)
+        inputs2 = torch.rand(2, 1, 128, 128)
+        net = resnet18(4, 3).eval()
+        outputs = net(inputs1, inputs2)
+        self.assertEqual(inputs1.size(), outputs.size())
+        net = resnet18(4, 3, batch_norm=False).eval()
+        outputs = net(inputs1, inputs2)
+        self.assertEqual(inputs1.size(), outputs.size())
 
 if __name__ == '__main__':
     unittest.main()
