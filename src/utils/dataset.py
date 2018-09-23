ccf8bcc3c5e83d4679dff046aca70b3f1dc03c49
Doi Kento <spreadcats.nya@gmail.com>
2018-09-23 14:23:58 +0900

    [fix] shape of array

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -15,6 +15,12 @@ class PansharpenDataset(data_utils.Dataset):
         data_path = str(self.data_paths[idx])
         data = joblib.load(data_path)
         data = [data['rgb'].astype(np.float32), data['b8'].astype(np.float32)]
+
+        rgb, b8 = data
+        if len(b8.shape) == 2:
+            b8 = b8[np.newaxis, ...]
+        data = (rgb, b8)
+
         if self.transforms is not None:
             data = self.transforms(data)
         return data
